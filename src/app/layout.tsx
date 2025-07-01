cc5f85edb41e53f1c0351bdc6d57f6498f94076a
srseen <seereen.2540@gmail.com>
2025-07-02 00:54:23 +0700

    feat: remove unused toast and tooltip components to streamline codebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import { ReactNode } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import "../styles/globals.css";
@@ -15,13 +12,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body>
         <QueryClientProvider client={queryClient}>
-          <LanguageProvider>
-            <TooltipProvider>
-              <Toaster />
-              <Sonner />
-              {children}
-            </TooltipProvider>
-          </LanguageProvider>
+          <LanguageProvider>{children}</LanguageProvider>
         </QueryClientProvider>
       </body>
     </html>
